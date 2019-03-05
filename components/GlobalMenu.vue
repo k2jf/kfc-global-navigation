945dca40eaea14e7d5cf99e8d4a6a6c9a0d41886
youli <youli@k2data.com.cn>
2019-03-05 15:10:59 +0800

    菜单拆分

--- components/GlobalMenu.vue
+++ components/GlobalMenu.vue
@@ -1,74 +1,34 @@
 <template>
   <Menu
-    :active-name="activeName"
-    :mode="mode"
-    :theme="theme"
+    :active-name="getActiveName"
+    mode="horizontal"
+    theme="dark"
     width="auto"
     @on-select="changeMenu"
   >
     <template v-for="submenu in getMenuList">
-      <MenuItem
-        :name="submenu.name"
-        v-if="!submenu.children || !isShowSubMenu"
-        :key="submenu.title">
+      <MenuItem :name="submenu.name" :key="submenu.name">
         <Icon :type="submenu.icon" />
         {{ submenu.title }}
       </MenuItem>
-      <Submenu
-        :name="submenu.title"
-        v-if="submenu.children && isShowSubMenu"
-        :key="submenu.title">
-        <template slot="title">
-          <Icon :type="submenu.icon" />
-          <span>{{ submenu.title }}</span>
-        </template>
-        <template v-for="child in submenu.children">
-          <MenuItem :name="child.name" :key="'menuitem' + child.name">
-            {{ child.title }}
-          </MenuItem>
-        </template>
-      </Submenu>
     </template>
   </Menu>
 </template>
 <script>
-import { Menu, MenuItem, Submenu, Icon } from 'iview'
+import { Menu, MenuItem, Icon } from 'iview'
 export default {
   name: 'GlobalMenu',
   components: {
     Menu,
     MenuItem,
-    Submenu,
     Icon
-  },
-  props: {
-    menuList: {
-      type: Array,
-      required: true
-    },
-    isShowSubMenu: {
-      type: Boolean,
-      required: true
-    },
-    mode: {
-      type: String,
-      required: false,
-      default: () => {
-        return 'vertical'
-      }
-    },
-    activeName: {
-      type: String,
-      required: true
-    },
-    theme: {
-      type: String,
-      required: true
-    }
   },
   computed: {
     getMenuList () {
-      return this.menuList
+      return this.$store.state.home.headerMenuList
+    },
+    getActiveName () {
+      return this.$store.state.home.headerActiveName
     }
   },
   methods: {
---
+++ components/SiderMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <Menu
+    :active-name="$route.name"
+    width="auto"
+    @on-select="onChangeMenu">
+    <template v-for="submenu in getMenuList">
+      <MenuItem
+        :name="submenu.name"
+        v-if="!submenu.children"
+        :key="submenu.title">
+        <Icon :type="submenu.icon" />
+        {{ submenu.title }}
+      </MenuItem>
+      <Submenu v-if="submenu.children" :key="submenu.name">
+        <template slot="title">
+          <Icon :type="submenu.icon" />
+          <span>{{ submenu.title }}</span>
+        </template>
+        <template v-for="child in submenu.children">
+          <MenuItem :name="child.name" :key="'menuitem' + child.name">
+            {{ child.title }}
+          </MenuItem>
+        </template>
+      </Submenu>
+    </template>
+  </Menu>
+</template>
+
+<script>
+import { Menu, MenuItem, Submenu, Icon } from 'iview'
+export default {
+  name: 'SiderMenu',
+  components: {
+    Menu,
+    MenuItem,
+    Submenu,
+    Icon
+  },
+  computed: {
+    getMenuList () {
+      return this.$store.state.home.siderMenuList
+    }
+  },
+  methods: {
+    onChangeMenu (name) {
+      // 跳转菜单对应页面
+      this.$router.push({
+        name: name
+      })
+      // 更新左侧高亮菜单名称
+      this.$store.commit('updateRouterName', name)
+    }
+  }
+}
+</script>
